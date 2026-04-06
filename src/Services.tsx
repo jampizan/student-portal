5b7ea2dcc596698f48fa5106cecbfdb1f4f81323
Jamaica Pizan <pizan_j@f112.ub.lan>
2026-04-06 09:48:53 +0800

    first commit

---
+++ src/Services.tsx
@@ -0,0 +1,10 @@
+export const Services = () => (
+        <div className="container mt-5">
+          <h1>Services</h1>
+          <p>Book Request Submission</p>
+          <p>Event Feedback Submission</p>
+          <p>Explore Academic Resources</p>
+        </div>
+      );
+  
+  export default Services;
